2c9a3679cc9418c54dec354aad7cc18deec6aa17
Luiz Guilherme Paro <lg_paro@hotmail.com>
2020-08-06 23:38:55 -0400

    adding Dockerfile and docker-compose.yml files

--- src/main/java/me/github/lparo/geolocation/config/RedisConfig.java
+++ src/main/java/me/github/lparo/geolocation/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package me.github.lparo.geolocation.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -14,10 +16,22 @@ import java.io.Serializable;
 @Profile("!integration-test")
 @Configuration
 public class RedisConfig {
+    private final String redisHost;
+    private final int redisPort;
+
+    public RedisConfig(@Value("${REDIS_HOST:localhost}") String redisHost,
+                       @Value("${REDIS_PORT:6379}") int redisPort) {
+        this.redisHost = redisHost;
+        this.redisPort = redisPort;
+    }
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        final RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+
+        return new JedisConnectionFactory(configuration);
     }
 
     @Bean
